257be99dd527c4c4a5751f21e4b4dae55aa99f64
Nicolas Erazo <nico.lacho@outlook.com>
2023-02-24 17:48:11 -0500

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WebPage
+# Webpage
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WebPage'`, () => {
+  it(`should have as title 'webpage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WebPage');
+    expect(app.title).toEqual('webpage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('WebPage app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('webpage app is running!');
   });
 });
